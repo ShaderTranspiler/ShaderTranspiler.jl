54ae17f5b92d646d2ab283caa9e9100529ad948c
szgeri <gerg.szucs@gmail.com>
2026-09-16 10:58:43 +0200

    Fix CLI target pointing to old export name

--- src/CLI.jl
+++ src/CLI.jl
@@ -3,10 +3,10 @@ module CLI
 using stc_jll
 
 function (@main)(ARGS)
-    stc_jll.stc_cli() do exe
+    stc_jll.stc() do exe
         cmd = `$exe $ARGS`
         proc = run(ignorestatus(cmd))
-        
+
         return proc.exitcode
     end
 end
